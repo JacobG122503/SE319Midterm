e336a9bf501a193ff5252907ea3e8135e2581acd
jacobg <jaybob1225@gmail.com>
2024-03-01 15:15:48 -0600

    Very close to finished with the conversion method. Can convert from any base to any base however I do not yet have support for using letters as a starting number to convert. Will work on that next.

--- conversion.js
+++ conversion.js
@@ -1,44 +1,75 @@
 //NOTE: Okay for JSON do like pastBase, newBase, pastNumber, newNumber
 
-function universalConvert() {
-    document.getElementById("uniOutput").style.color = "mediumseagreen";
-    let toConvert = parseInt(document.getElementById('uniUserInput').value);
-    let toBase = parseInt(document.getElementById('uniBase').value);
+
+function convert() {
+    //Steps: 
+    //NOTE: Check if number typed is actually of the correct fromBase
+    //1. Convert to base ten
+    //2. send to baseTenToAny
+    //3. Return that number
+    document.getElementById("output").style.color = "mediumseagreen";
+    let fromBase = parseInt(document.getElementById('fromBase').value);
+    let toConvert = document.getElementById('toConvert').value;
+    if (fromBase <= 10) toConvert = parseInt(toConvert);
+    let toBase = parseInt(document.getElementById('toBase').value);
+
+    if ((isNaN(toConvert) && fromBase <= 10) || isNaN(toBase) || isNaN(fromBase)) {
+        document.getElementById("output").style.color = "red";
+        document.getElementById("output").innerText = "ERROR: Type numbers correctly";
+        return;
+    }
+
+    if ((toBase < 2 || toBase > 36) || (fromBase < 2 || fromBase > 36)) {
+        document.getElementById("output").style.color = "red";
+        document.getElementById("output").innerText = "ERROR: Please type a base between 2 and 36";
+        return;
+    }
+
+    //Convert to base ten
+    if (fromBase != 10) toConvert = anyToBaseTen(toConvert, fromBase);
+
+    if (toBase != 10) toConvert = baseTenToAny(toConvert, toBase);
+
+    document.getElementById('output').innerText = toConvert;
+}
+
+function anyToBaseTen(toConvert, fromBase) {
+    let result = 0;
+    //Change to string so I can iterate through
+    toConvert = toConvert.toString();
+
+    for (let i = toConvert.length - 1, j = 0; i >= 0; i--, j++) {
+        if (fromBase > 10) toConvert[j] = letterToNumber(toConvert[j]);
+        result += toConvert[j] * (Math.pow(fromBase, i));
+    }
+
+    return result;
+}
+
+function baseTenToAny(toConvert, toBase) {
     let result = "";
-
-    if (isNaN(toConvert) || isNaN(toBase)) {
-        document.getElementById("uniOutput").style.color = "red";
-        document.getElementById("uniOutput").innerText = "ERROR: Type numbers correctly";
-        return;
-    }
-
-    if (toBase < 2 || toBase > 36) {
-        document.getElementById("uniOutput").style.color = "red";
-        document.getElementById("uniOutput").innerText = "ERROR: Please type a base between 2 and 36";
-        return;
-    }
 
     while (toConvert !== 0) {
         let remainder = toConvert % toBase;
-        result = (toBase > 10) ? (convertToLetter(remainder) + result) : (remainder + result);
+        result = (toBase > 10) ? (numberToLetter(remainder) + result) : (remainder + result);
 
         toConvert = Math.floor(toConvert / toBase);
     }
 
     if (result === "") result = "0";
 
-    document.getElementById('uniOutput').innerText = result;
+    return result;
 }
 
 
 function handleKeyPress(event, type) {
     // Check if the key pressed is Enter (key code 13)
-    if (event.keyCode === 13 && type == "uni") {
-        universalConvert();
+    if (event.keyCode === 13) {
+        convert();
     }
 }
 
-function convertToLetter(number) {
+function numberToLetter(number) {
     const letters = {
         '10': 'A',
         '11': 'B',
@@ -72,5 +103,42 @@ function convertToLetter(number) {
         return letters[number];
     } else {
         return number;
+    }
+}
+
+function letterToNumber(letter) {
+    const letters = {
+        'A': '10',
+        'B': '11',
+        'C': '12',
+        'D': '13',
+        'E': '14',
+        'F': '15',
+        'G': '16',
+        'H': '17',
+        'I': '18',
+        'J': '19',
+        'K': '20',
+        'L': '21',
+        'M': '22',
+        'N': '23',
+        'O': '24',
+        'P': '25',
+        'Q': '26',
+        'R': '27',
+        'S': '28',
+        'T': '29',
+        'U': '30',
+        'V': '31',
+        'W': '32',
+        'X': '33',
+        'Y': '34',
+        'Z': '35'
+    };
+
+    if (letters.hasOwnProperty(letter)) {
+        return letters[letter];
+    } else {
+        return letter;
     }
 }
